f20b8eda0486acf1a0aed9bed8d5f24e93eb2443
Sheikh Javeed <sheikhmannanjaveed@gmail.com>
2024-05-25 22:21:49 +0530

    added java files

---
+++ basic2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class basic2 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int sum=a+b;
+        System.out.println(sum);
+        sc.close(); //The close() is a method of Java Scanner class which is used to closes the scanner.
+    }
+}
